5f7988cecb2155d781cd6013399433f81889404f
Manav Darji <manavdarji.india@gmail.com>
2021-12-22 14:53:04 +0530

    move merkle functions to new file

--- cmd/rpcdaemon/commands/bor_snapshots.go
+++ cmd/rpcdaemon/commands/bor_snapshots.go
@@ -381,53 +381,6 @@ func ecrecover(header *types.Header, c *params.BorConfig) (common.Address, error
 	return signer, nil
 }
 
-func appendBytes32(data ...[]byte) []byte {
-	var result []byte
-	for _, v := range data {
-		paddedV, err := convertTo32(v)
-		if err == nil {
-			result = append(result, paddedV[:]...)
-		}
-	}
-	return result
-}
-
-func nextPowerOfTwo(n uint64) uint64 {
-	if n == 0 {
-		return 1
-	}
-	// http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
-	n--
-	n |= n >> 1
-	n |= n >> 2
-	n |= n >> 4
-	n |= n >> 8
-	n |= n >> 16
-	n |= n >> 32
-	n++
-	return n
-}
-
-func convertTo32(input []byte) (output [32]byte, err error) {
-	l := len(input)
-	if l > 32 || l == 0 {
-		return
-	}
-	copy(output[32-l:], input[:])
-	return
-}
-
-func convert(input []([32]byte)) [][]byte {
-	var output [][]byte
-	for _, in := range input {
-		newInput := make([]byte, len(in[:]))
-		copy(newInput, in[:])
-		output = append(output, newInput)
-
-	}
-	return output
-}
-
 // safe addition
 func safeAdd(a, b int64) (int64, bool) {
 	if b > 0 && a > math.MaxInt64-b {
